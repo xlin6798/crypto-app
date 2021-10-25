7c344987f79463b2c0877a6d40a0044aa116ba58
xlin6798 <xlin6798@gmail.com>
2021-10-25 12:22:53 -0400

    Add support for local save and change overall layout

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import { FaGithub, FaLinkedin} from "react-icons/fa";
+import Container from "react-bootstrap/Container";
+
+function Footer() {
+    return (
+        <footer className="bg-white">
+            <Container>
+                <p className="text-center text-muted py-3 m-0">
+                    <a className="text-muted" href="https://github.com/xlin6798/xlin6798.github.io">
+                        © 2021 Xinyi Lin Created with React
+                    </a>
+                    {" "}
+                    <a
+                        href="https://github.com/xlin6798"
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="My github"
+                    >
+                        <FaGithub />
+                    </a>
+                    {" "}
+                    <a
+                        href="https://www.linkedin.com/in/xinyi-lin-xlin6798/"
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="My linkedin"
+                    >
+                        <FaLinkedin />
+                    </a>
+                </p>
+            </Container>
+        </footer>
+    );
+}
+
+export default Footer;
